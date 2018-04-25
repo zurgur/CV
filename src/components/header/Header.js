e8932c5a42663b1fd667908929dd6e076dfc5834
zurgur <harryfreyr@gmail.com>
2018-04-25 19:03:10 +0000

    gera info texta

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,6 +11,12 @@ export default class Header extends Component {
             <header className="App-header">
                 <h1 className="App-title">Alexander Freyr Sveinsson</h1>
                 <img className='meImg' src='me.jpg'/>
+                <div className='infoTex'>
+                    <p className='infoP'>Stakhamrar 19</p>
+                    <p className='infoP'>Reykjavík, 112 Rvk</p>
+                    <p className='infoP'>(234) 869-2855</p>
+                    <p className='infoP'>alexfreyrs@gmail.com</p>
+                </div>
                 <div className='profileLinks'>
                     <a href='https://github.com/zurgur' className='git' ><img src='GitHub-Mark-120px-plus.png' className='headImg' /></a>
                     <a href='https://www.linkedin.com/in/alexander-sveinsson-a36715139/' className='ln' ><img src='In-2C-128px-TM.png' className='headImg' /></a>
