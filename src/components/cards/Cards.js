ecd9e35d5eb389f4577a7b2d6a5124c1755f42f2
zurgur <harryfreyr@gmail.com>
2018-05-24 23:14:10 +0000

    card

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -16,7 +16,6 @@ export default class Projects extends Component{
             <div className='Cards'>
             {info.map((i) => (
                 <a key={i.name} className='box' href={i.page}>
-                    {i.image ? <img className='cardImg' src={i.image} alt='card'/> : <img alt='card' className='cardImg--empty' src=''/>}
                     <p className='cardName'>{i.name}</p>
                     <p className='cardTittle'>{i.tittle}</p>
                 </a>
