ed0ebb7d6aacf0b9bf13a7da2ca87c5305c22924
zurgur <harryfreyr@gmail.com>
2018-07-27 21:02:29 +0000

    made the mentun and work more responcive

--- src/components/activity/Activity.js
+++ src/components/activity/Activity.js
@@ -15,11 +15,11 @@ export default class Activity extends Component{
                 {about.map((thing) => (
                     <div className='currentAct' key={thing.tittle}>
                         <div className='Actnames'>
+                        <div className='titleAndInf'>
                             <h2 className='title'>{thing.tittle}</h2>
                             <p className='info'>{thing.info}</p>
-                        </div>
-                        <div className='date'>
-                            <p>{thing.date}</p>
+                            </div>
+                                <p>{thing.date}</p>
                         </div>
                     </div>
                 ))}
