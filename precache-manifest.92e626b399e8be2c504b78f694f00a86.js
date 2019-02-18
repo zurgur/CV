fb142ead2c6d4bb45784061e94ec0909b3df971e
unknown <harryfreyr@gmail.com>
2019-02-18 22:38:15 +0000

    Updates

---
+++ precache-manifest.92e626b399e8be2c504b78f694f00a86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d23b6a8d3b851aa1065",
+    "url": "./static/css/main.f9848e60.chunk.css"
+  },
+  {
+    "revision": "2d23b6a8d3b851aa1065",
+    "url": "./static/js/main.2d23b6a8.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "083633f5ecc7d583379e",
+    "url": "./static/js/2.083633f5.chunk.js"
+  },
+  {
+    "revision": "5f188d41b919d2b3c0bee1527ecb2cd1",
+    "url": "./index.html"
+  }
+];
