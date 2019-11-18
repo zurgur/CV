e840e32b1ff18644664e4cec83bd58a404a31b67
unknown <harryfreyr@gmail.com>
2019-11-18 21:22:47 +0000

    Updates

---
+++ precache-manifest.10154095bcd2597411df5fae2325d076.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e388f835cfc33c0e9f71cc18c0cf3853",
+    "url": "./index.html"
+  },
+  {
+    "revision": "13b2dc3c00bda25562d4",
+    "url": "./static/css/main.d47c46ea.chunk.css"
+  },
+  {
+    "revision": "ab0a560b5f16fe4cac6f",
+    "url": "./static/js/2.301668fc.chunk.js"
+  },
+  {
+    "revision": "13b2dc3c00bda25562d4",
+    "url": "./static/js/main.73d199bb.chunk.js"
+  },
+  {
+    "revision": "56a6b2399647f3d207bd",
+    "url": "./static/js/runtime-main.f1c4a8af.js"
+  }
+]);
