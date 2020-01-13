efca04c5e09e1a50fc73307e2dc0dfd4d540617e
unknown <harryfreyr@gmail.com>
2020-01-13 21:32:26 +0000

    left align text

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -7,9 +7,11 @@ export default (props) => {
   return (
     <div className="Footer">
       <h1 className="footer">{name}</h1>
-      <a href="mailto:alexfreyrs@gmail.com?Subject=Hello%20again"><img src="email.png" alt="email" className="headImg" /></a>
-      <a href="https://github.com/zurgur" className="git"><img src="GitHub-Mark-120px-plus.png" className="headImg" alt="github" /></a>
-      <a href="https://www.linkedin.com/in/alexander-sveinsson-a36715139/" className="ln"><img src="In-2C-128px-TM.png" className="headImg" alt="linkedinn" /></a>
+      <div className="Footer-links">
+        <a href="mailto:alexfreyrs@gmail.com?Subject=Hello%20again"><img src="email.png" alt="email" className="email" /></a>
+        <a href="https://github.com/zurgur" className="git"><img src="GitHub-Mark-120px-plus.png" className="email" alt="github" /></a>
+        <a href="https://www.linkedin.com/in/alexander-sveinsson-a36715139/" className="ln"><img src="In-2C-128px-TM.png" className="email" alt="linkedinn" /></a>
+      </div>
     </div>
   );
 };
